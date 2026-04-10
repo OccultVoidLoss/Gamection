2592581adc63c7f295edb4baacd7bca3f1a9f3dd
Void <luisgustavoromano@gmail.com>
2026-04-10 09:49:30 -0300

    Add files via upload

---
+++ trabalho primeiro bimestre/blueprints/users/routes.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from . import user_bp
+from .controllers import UserController
+
+
+
+controller = UserController()
+
+
+
+@user_bp.route("/cadastro", methods=["GET", "POST"])
+def cadastrar_user():
+    if request.method == "POST":
+        return controller.cadastrar_user()
+
+    return controller.preparar_cadastro()
+
+@user_bp.route("/")
+def index():
+    return controller.index()
+
+
+@user_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return controller.login()
+
+    return controller.preparar_login()
+
+@user_bp.route("/sair", methods=["POST", "GET"])
+def sair():
+    return controller.encerrar()
+
+@user_bp.route("/erro", methods=["POST", "GET"])
+def erro():
+    return controller.erro()
+
+
+#@user_bp.route("/editar/<int:id>", methods=["GET", "POST"])
+#def editar_jogo(id):
+#    if request.method == "POST":
+#        return controller.editar_jogo(id)
+#    return controller.preparar_edicao(id)
+
+
+#@user_bp.route("/excluir/<int:id>", methods=["POST"])
+#def excluir_livro(id):
+#    return controller.remover_jogo(id)
---
+++ trabalho primeiro bimestre/script.sql
@@ -0,0 +1,24 @@
+USE games_db;
+CREATE TABLE IF NOT EXISTS Jogos(
+	id INT auto_increment PRIMARY KEY,
+    Titulo VARCHAR(50),
+    Desenvolvedora VARCHAR (50),
+    Data_lanc DATE,
+    Genero VARCHAR(50),
+    Sinopse VARCHAR (1000),
+    Plataformas VARCHAR (50),
+    Imagem LONGBLOB ,
+    Categoria VARCHAR (50)
+);
+
+CREATE TABLE IF NOT EXISTS Usuario(
+		id INT auto_increment PRIMARY KEY,
+		Email VARCHAR(50),
+        Senha VARCHAR(50)
+);
+
+
+CREATE TABLE IF NOT EXISTS Comentarios(
+		Autor VARCHAR(50),
+        Comentario VARCHAR(50)
+);
