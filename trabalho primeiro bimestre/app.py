74d756cb788d19d5a1740312b72255b2e3609f73
Void <luisgustavoromano@gmail.com>
2026-04-10 14:40:30 -0300

    Add files via upload

---
+++ trabalho primeiro bimestre/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for
+from blueprints.games import games_bp
+from blueprints.users import user_bp
+from dotenv import load_dotenv 
+import os
+load_dotenv()
+
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")  # Necessário para usar o 'flash'
+
+
+app.register_blueprint(games_bp)
+app.register_blueprint(user_bp)
+
+@app.route("/")
+def index():
+    return redirect(url_for("games.listar_jogos"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
